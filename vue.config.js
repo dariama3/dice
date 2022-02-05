573ca2756c93b0edd9a4f30abbfecfe157cc9902
Daria Malenko <malienko.99@ukr.net>
2022-02-05 18:09:32 +0200

    Meta title and description are added

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production'
-}
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Dice',
+      description: 'Application to roll the dice by click or shake'
+    }
+  }
+};
